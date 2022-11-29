fd8074fd6be6f188df46042bd5c0098ecb0eacd2
hyejin <obndo8539@naver.com>
2022-11-29 19:05:53 +0900

    첫수업, kakaomap 불러오기

---
+++ src/main/java/com/kakao/map/controller/PageLoadTestController.java
@@ -0,0 +1,32 @@
+package com.kakao.map.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PageLoadTestController {
+
+    @RequestMapping(value = "/t1", method = RequestMethod.GET)
+    public String test1() {
+        System.out.println("t1 요청 들어옴");
+        return "test1";
+    }
+    @RequestMapping(value = "/t2", method = RequestMethod.GET)
+    public String test2() {
+        System.out.println("t2 요청 들어옴");
+        return "test1";
+    }
+
+    @RequestMapping(value = "/test/page", method = RequestMethod.GET)
+    public String test3() {
+//        System.out.println("t2 요청 들어옴");
+        return "test/test2";
+    }
+
+    @RequestMapping(value = "/map", method = RequestMethod.GET)
+    public String test4() {
+//        System.out.println("t2 요청 들어옴");
+        return "map";
+    }
+}
